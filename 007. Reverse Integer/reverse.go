2edd1afc9466e0d7bd112a1591d88c0456c21402
FENGL-CN <bigwindlee@hotmail.com>
2017-09-22 14:21:14 +0800

    Reverse digits of an integer.
    Example1: x = 123, return 321
    Example2: x = -123, return -321

---
+++ 007. Reverse Integer/reverse.go
@@ -0,0 +1,18 @@
+package reverse
+
+func reverse(x int) int {
+  sign := 1
+  if x < 0 {
+    sign = -1
+    x *= -1
+  }
+  y := 0
+  for x > 0 {
+    y = y*10 + x%10
+    x /= 10
+  }
+  if y > 0x7FFFFFFF {
+    return 0
+  }
+  return y*sign
+}
---
+++ 007. Reverse Integer/reverse_test.go
@@ -0,0 +1,10 @@
+package reverse
+
+import (
+  "fmt"
+  "testing"
+)
+
+func TestReverse(t *testing.T){
+  fmt.Println(reverse(0x7fffffff))
+}
