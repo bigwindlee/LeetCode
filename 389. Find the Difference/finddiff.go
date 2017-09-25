2d0b26a35278298d998bf38a4de9968760639f18
ThinkPad-S3 <bigwindlee@hotmail.com>
2017-09-26 00:01:46 +0800

    389. Find the Difference

---
+++ 389. Find the Difference/finddiff.go
@@ -0,0 +1,13 @@
+package finddiff
+
+/* Score: 69.57 */
+func findTheDifference(s string, t string) byte {
+    var xor rune
+    for _, char := range s {
+        xor ^= char
+    }
+    for _, char := range t {
+        xor ^= char
+    }
+    return byte(xor)
+}
