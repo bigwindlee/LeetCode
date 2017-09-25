f795a46d34a2e1a8309984c45c35f21c61623a7f
ThinkPad-S3 <bigwindlee@hotmail.com>
2017-09-26 00:30:48 +0800

    389. Find the Difference, gofmt -w

--- 389. Find the Difference/finddiff.go
+++ 389. Find the Difference/finddiff.go
@@ -2,12 +2,12 @@ package finddiff
 
 /* Score: 69.57 */
 func findTheDifference(s string, t string) byte {
-    var xor rune
-    for _, char := range s {
-        xor ^= char
-    }
-    for _, char := range t {
-        xor ^= char
-    }
-    return byte(xor)
+	var xor rune
+	for _, char := range s {
+		xor ^= char
+	}
+	for _, char := range t {
+		xor ^= char
+	}
+	return byte(xor)
 }
