bef70b28b06fd87d4910b1836b643777b00867dd
ThinkPad-S3 <bigwindlee@hotmail.com>
2017-10-11 17:42:01 +0800

    067. Add Binary | Improve

--- 067. Add Binary/addbinary.go
+++ 067. Add Binary/addbinary.go
@@ -4,7 +4,7 @@ func addBinary(a string, b string) string {
 	i, j := len(a)-1, len(b)-1
 	var ax, bx, carry, sum byte
 	c := make([]byte, 0)
-	for i >= 0 || j >= 0 {
+	for i >= 0 || j >= 0 || carry == 1 {
 		if i < 0 {
 			ax = 0
 		} else {
@@ -16,14 +16,11 @@ func addBinary(a string, b string) string {
 			bx = b[j] - '0'
 		}
 		sum = ax + bx + carry
-		carry = sum >> 1
-		c = append(c, '0'+sum&0x1)
+		carry = sum >> 1         // MSD of sum
+		c = append(c, '0'+sum%2) // LSD of sum
 
 		i--
 		j--
-	}
-	if carry == 1 {
-		c = append(c, '1')
 	}
 	for i := 0; i < len(c)/2; i++ { // reverse the slice
 		c[i], c[len(c)-1-i] = c[len(c)-1-i], c[i]
