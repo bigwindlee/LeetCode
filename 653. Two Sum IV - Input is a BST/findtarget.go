b9966c83f4c8c2eb89d3ad5d701a4ca32635151b
ThinkPad-S3 <bigwindlee@hotmail.com>
2017-10-16 23:27:48 +0800

    653. Two Sum IV - Input is a BST

--- 653. Two Sum IV - Input is a BST/findtarget.go
+++ 653. Two Sum IV - Input is a BST/findtarget.go
@@ -1,26 +1,60 @@
 package find2sum
 
 import (
-	"fmt"
 	. "leetcomm"
 )
 
 func findTarget(root *TreeNode, k int) bool {
-	nums := make([]int, 0)
-	return dfs(root, k, &nums)
+	options := []func(root *TreeNode, k int) bool{findTarget0, findTarget1}
+	return options[1](root, k)
 }
 
-func dfs(root *TreeNode, k int, nums *[]int) bool {
+/*-----------------------------------------------------
+            Method 1
+-----------------------------------------------------*/
+func findTarget1(root *TreeNode, k int) bool {
+	nums := []int{}
+	dfs1(root, &nums)
+	for i, j := 0, len(nums)-1; i < j; {
+		sum := nums[i] + nums[j]
+		if sum == k {
+			return true
+		} else if sum > k {
+			j--
+		} else {
+			i++
+		}
+	}
+	return false
+}
+
+func dfs1(root *TreeNode, nums *[]int) {
+	if root == nil {
+		return
+	}
+	dfs1(root.Left, nums)
+	*nums = append(*nums, root.Val)
+	dfs1(root.Right, nums)
+}
+
+/*-----------------------------------------------------
+            Method 0
+-----------------------------------------------------*/
+func findTarget0(root *TreeNode, k int) bool {
+	nums := make([]int, 0)
+	return dfs0(root, k, &nums)
+}
+
+func dfs0(root *TreeNode, k int, nums *[]int) bool {
 	if root == nil {
 		return false
 	}
-	lfound := dfs(root.Left, k, nums)
-	fmt.Printf("curr: %d, nums: %v, k: %d\n", root.Val, *nums, k)
+	lfound := dfs0(root.Left, k, nums)
 	if binarysearch(*nums, k-root.Val) {
 		return true
 	}
 	*nums = append(*nums, root.Val)
-	return lfound || dfs(root.Right, k, nums)
+	return lfound || dfs0(root.Right, k, nums)
 }
 
 func binarysearch(nums []int, target int) bool {
