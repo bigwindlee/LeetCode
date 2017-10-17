9f911eb41a77f91bc3094197ec42af3290fcb29a
ThinkPad-S3 <bigwindlee@hotmail.com>
2017-10-17 17:34:54 +0800

    167. Two Sum II - Input array is sorted

---
+++ 167. Two Sum II - Input array is sorted/twosum2.go
@@ -0,0 +1,16 @@
+package twosum2
+
+func twoSum(numbers []int, target int) []int {
+	i, j := 0, len(numbers)-1
+	for i < j {
+		sum := numbers[i] + numbers[j]
+		if sum < target {
+			i++
+		} else if sum == target {
+			return []int{i + 1, j + 1}
+		} else {
+			j--
+		}
+	}
+	return nil
+}
