291b9dd1ab333d5842abb8e3d9a00913ef1700db
ThinkPad-S3 <bigwindlee@hotmail.com>
2017-11-14 00:00:12 +0800

    118. Pascal's Triangle | Dynamic Programming

--- 118. Pascal's Triangle/generate.go
+++ 118. Pascal's Triangle/generate.go
@@ -18,6 +18,28 @@ Return
 */
 
 func generate(numRows int) [][]int {
+	options := []func(numRows int) [][]int{generate0, generate1}
+	return options[1](numRows)
+}
+
+func generate1(numRows int) [][]int {
+	res := make([][]int, numRows)
+	for i := 0; i < numRows; i++ {
+		res[i] = make([]int, i+1)
+	}
+	for i := 0; i < numRows; i++ {
+		for j := 0; j <= i; j++ {
+			if j == 0 || j == i {
+				res[i][j] = 1
+			} else {
+				res[i][j] = res[i-1][j] + res[i-1][j-1]
+			}
+		}
+	}
+	return res
+}
+
+func generate0(numRows int) [][]int {
 	if numRows == 0 {
 		return [][]int{}
 	} else if numRows == 1 {
