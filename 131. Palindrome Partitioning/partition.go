bdd9701c9df6b38ee60fe8fe439263a2d934d0b9
ThinkPad-S3 <bigwindlee@hotmail.com>
2017-11-25 01:15:12 +0800

    131. Palindrome Partitioning

---
+++ 131. Palindrome Partitioning/partition.go
@@ -0,0 +1,34 @@
+package partition
+
+func partition(s string) [][]string {
+	var res [][]string
+	var path []string
+	backtrack(s, &path, &res)
+	return res
+}
+
+func backtrack(s string, path *[]string, res *[][]string) {
+	if len(s) == 0 {
+		tmp := make([]string, len(*path))
+		copy(tmp, *path)
+		*res = append(*res, tmp)
+		return
+	}
+	for i := 1; i <= len(s); i++ {
+		if isValid(s[:i]) {
+			*path = append(*path, s[:i])
+			backtrack(s[i:], path, res)
+			*path = (*path)[:len(*path)-1]
+		}
+	}
+}
+
+func isValid(sub string) bool {
+	L := len(sub)
+	for i := 0; i < L/2; i++ {
+		if sub[i] != sub[L-1-i] {
+			return false
+		}
+	}
+	return true
+}
---
+++ 131. Palindrome Partitioning/partition_test.go
@@ -0,0 +1,12 @@
+package partition
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestPartition(t *testing.T) {
+	fmt.Printf("%v\n", partition("cbbbcc"))
+	// fmt.Printf("%v\n", partition("abcba"))
+
+}
