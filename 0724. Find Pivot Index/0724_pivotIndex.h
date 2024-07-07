5ae9677c35cded44906036974b587ee436822ccb
bigwindlee <bigwindlee@icloud.com>
2024-07-08 01:55:35 +0800

    Update 0724_pivotIndex.h

--- 0724. Find Pivot Index/0724_pivotIndex.h
+++ 0724. Find Pivot Index/0724_pivotIndex.h
@@ -9,7 +9,7 @@ using namespace std;
 左右侧元素和相等即为：
 sum（左）+ nums[i] + sum（右）= total
 也就是：
-2 * sum + nums[i] = total
+2 * sum + nums[i] = total  // 理解了这一点，就不需要把前缀和存在数组里！
 
 当中心索引左侧或右侧没有元素时，即为零个项相加，这在数学上称作「空和」（empty sum）。在程序设计中我们约定「空和是零」。
 */
