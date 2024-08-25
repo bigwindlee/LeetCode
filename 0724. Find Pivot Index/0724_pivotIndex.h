a8a01f25ec3e4cf256ace96873b21872b9dd9b88
bigwindlee <bigwindlee@qq.com>
2024-08-25 13:16:19 +0800

    0724. Find Pivot Index

--- 0724. Find Pivot Index/0724_pivotIndex.h
+++ 0724. Find Pivot Index/0724_pivotIndex.h
@@ -1,10 +1,17 @@
 ﻿#pragma once
+#include <numeric>
 #include <vector>
 using namespace std;
 
 /*
+* 724. 寻找数组的中心下标
+给你一个整数数组 nums ，请计算数组的 中心下标 。
+数组 中心下标 是数组的一个下标，其左侧所有元素相加的和等于右侧所有元素相加的和。
+如果中心下标位于数组最左端，那么左侧数之和视为 0 ，因为在下标的左侧不存在元素。这一点对于中心下标位于数组最右端同样适用。
+如果数组有多个中心下标，应该返回 最靠近左边 的那一个。如果数组不存在中心下标，返回 -1 。
+
+思路分析：
 方法一：前缀和
-思路
 记数组的全部元素之和为 total，当遍历到第 i 个元素时，设其左侧元素之和为 sum，则其右侧元素之和为 total - sum - nums[i]
 左右侧元素和相等即为：
 sum（左）+ nums[i] + sum（右）= total
@@ -18,19 +25,14 @@ class Solution {
 public:
     int pivotIndex(vector<int>& nums)
     {
-        int total = 0, presum = 0;
-
-        for (int num : nums) {
-            total += num;
-        }
-
+        int total = accumulate(nums.begin(), nums.end(), 0);
+        int pre_sum = 0;
         for (int i = 0; i < nums.size(); ++i) {
-            if (2 * presum + nums[i] == total) {
+            if (pre_sum * 2 + nums[i] == total) {
                 return i;
             }
-            presum += nums[i];
+            pre_sum += nums[i];
         }
-
         return -1;
     }
 };
