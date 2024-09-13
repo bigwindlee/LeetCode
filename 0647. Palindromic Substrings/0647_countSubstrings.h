506fe9593e15fe32a1d4084ddb1a5ddeae4f0a84
bigwindlee <bigwindlee@qq.com>
2024-09-14 00:09:26 +0800

    647. 回文子串

---
+++ 0647. Palindromic Substrings/0647_countSubstrings.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+#include <string>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int countSubstrings(string s)
+    {
+        int ans = 0, n = s.size();
+        // dp[i][j] 表示 s[i:j] 是否是回文；
+        // 默认不是回文，只需找出是回文的子串；
+        vector<vector<bool>> dp(n, vector<bool>(n, false));
+
+        // 从下往上填表（i从大到小，j从左往右）
+        for (int i = n - 1; i >= 0; --i) {
+            for (int j = i; j < n; ++j) {
+                if (s[i] == s[j]) {
+                    // 单个字符或两个字符是回文，不需要提前填充对角线；
+                    // 长度大于2时，取决于内部子串（边界内收）是否是回文；
+                    dp[i][j] = (j - i < 2) ? true : dp[i + 1][j - 1];
+                    ans += dp[i][j] ? 1 : 0;
+                }
+            }
+        }
+
+        return ans;
+    }
+};
