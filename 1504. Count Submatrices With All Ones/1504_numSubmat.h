283ceb3b804230e2930d3e21ffba9f872534235d
bigwindlee <bigwindlee@icloud.com>
2024-10-12 01:28:55 +0800

    Update 1504_numSubmat.h

--- 1504. Count Submatrices With All Ones/1504_numSubmat.h
+++ 1504. Count Submatrices With All Ones/1504_numSubmat.h
@@ -25,21 +25,16 @@ public:
     int numSubmat(vector<vector<int>>& mat)
     {
         int m = mat.size(), n = mat[0].size();
-        vector<vector<int>> dp(m, vector<int>(n, 0)); // dp[i][j]表示从(i, j)开始往左，连续1的数量；也就是以(i, j)为右下角的宽度！
+        vector<vector<int>> dp(m, vector<int>(n)); // dp[i][j]表示从(i, j)开始往左，连续1的数量；也就是以(i, j)为右下角的宽度！
 
         // 计算每个元素左侧连续的1的数量，包括自己
+        int ans = 0;
         for (int i = 0; i < m; ++i) {
             for (int j = 0; j < n; ++j) {
                 if (mat[i][j] == 1) {
                     dp[i][j] = (j == 0) ? 1 : dp[i][j - 1] + 1; // 递推计算宽度！
                 }
-            }
-        }
 
-        int ans = 0;
-        // 枚举(i, j)，计算以每个(i, j)为右下角的全1子矩形数量；累加到ans；
-        for (int j = 0; j < n; ++j) {
-            for (int i = 0; i < m; ++i) {
                 int minWidth = dp[i][j];
                 for (int k = i; k >= 0; --k) { // 行数k递减[i -> 0]，相当于以(i, j)为底座的层高递增；遍历所有层高；
                     if (dp[k][j] == 0)
@@ -49,7 +44,6 @@ public:
                 }
             }
         }
-
         return ans;
     }
 };
