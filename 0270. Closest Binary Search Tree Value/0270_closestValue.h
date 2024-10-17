d2855898cb3c1553d38f8e8c43b99234b50aad24
bigwindlee <bigwindlee@qq.com>
2024-10-18 01:47:56 +0800

    270. 最接近的二叉搜索树值

---
+++ 0270. Closest Binary Search Tree Value/0270_closestValue.h
@@ -0,0 +1,35 @@
+﻿#pragma once
+#include "TreeNode.h"
+#include <cstdlib>
+using namespace std;
+
+/*
+270. 最接近的二叉搜索树值
+给你二叉搜索树的根节点 root 和一个目标值 target ，请在该二叉搜索树中找到最接近目标值 target 的数值。如果有多个答案，返回最小的那个。
+*/
+
+/*
+思路分析：二叉搜索树
+二叉搜索树的特征非常明显：大了往右找，小了往左找；
+*/
+class Solution {
+public:
+    int closestValue(TreeNode* root, double target)
+    {
+        int ans = root->val;
+        double bias = abs(target - ans);
+
+        TreeNode* p = root;
+        while (p) {
+            double nb = target - p->val; // new bias
+            if (abs(nb) < bias) { // 偏移更小，更接近；
+                bias = abs(nb);
+                ans = p->val;
+            } else if (abs(nb) == bias && p->val < ans) { // 多个答案，返回最小的
+                ans = p->val;
+            }
+            p = (nb > 0) ? p->right : p->left; // 大了往右找，小了往左找
+        }
+        return ans;
+    }
+};
