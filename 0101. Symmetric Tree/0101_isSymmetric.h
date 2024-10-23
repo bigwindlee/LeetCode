00aa7a22a4f47b36fd76ce86aa870f242acd62d2
bigwindlee <bigwindlee@qq.com>
2024-10-23 12:17:54 +0800

    101. 对称二叉树

---
+++ 0101. Symmetric Tree/0101_isSymmetric.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+#include "TreeNode.h"
+
+/*
+ 思路分析：二叉树DFS，但有技巧！
+ 如果左树的左孩子与右树的右孩子对称，左树的右孩子与右树的左孩子对称，那么这个左树和右树就对称。
+ */
+class Solution {
+public:
+    bool isSymmetric(TreeNode* root)
+    {
+        // 树中节点数目在范围 [1, 1000] 内，root不为空
+        return dfs(root->left, root->right);
+    }
+
+    bool dfs(TreeNode* left, TreeNode* right)
+    {
+        if (!left || !right) {
+            return left == right;
+        }
+        return left->val == right->val && dfs(left->left, right->right) && dfs(left->right, right->left);
+    }
+};
