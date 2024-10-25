14279ecf31ea46688c259bb4ca44a0d369dfcd62
bigwindlee <bigwindlee@icloud.com>
2024-10-26 01:47:40 +0800

    Update 0722_removeComments.h

--- 0722. Remove Comments/0722_removeComments.h
+++ 0722. Remove Comments/0722_removeComments.h
@@ -4,36 +4,36 @@
 using namespace std;
 
 /*
-ʹ������������inBlockComment ��ʾ�Ƿ��ڶ���ע���У�newLine ���ڹ�����ǰ�е����ݡ�
-����ÿһ��Դ���룬����ַ�������
-    - �������`����ע�Ϳ�ʼ��־`����������ע��ģʽ��
-    - �������`����ע�ͽ�����־`�����˳�����ע��ģʽ��
-    - ������� //������Ը��к�������ݡ�
-    - �������ע���У����ַ����ӵ� newLine �С�
-�����ǰ�в���ע������ newLine �ǿգ��������ӵ�����С�
-���ش�����Ĵ����С�
+使用两个变量：inBlockComment 表示是否在多行注释中，newLine 用于构建当前行的内容。
+遍历每一行源代码，逐个字符处理：
+    - 如果遇到`多行注释开始标志`，则进入多行注释模式。
+    - 如果遇到`多行注释结束标志`，则退出多行注释模式。
+    - 如果遇到 //，则忽略该行后面的内容。
+    - 如果不在注释中，则将字符添加到 newLine 中。
+如果当前行不在注释中且 newLine 非空，则将其添加到结果中。
+返回处理后的代码行。
 
-����ؼ�������4��״̬��
-1���������ע��ģʽ��
-2���˳�����ע��ģʽ��
-3��������ע�ͷ�����ȡ����һ�е�ɨ�裻
-4����ǰ��ע�ͣ�����ַ��ռ���
+本题关键是理解4中状态：
+1）进入多行注释模式；
+2）退出多行注释模式；
+3）遇单行注释符，提前结束一行的扫描；
+4）当前非注释，逐个字符收集；
 */
 class Solution_0722 {
 public:
     vector<string> removeComments(vector<string>& source)
     {
         vector<string> ans;
-        bool inBlockComment = false; // ��ǰ�Ƿ��ڶ���ע��ģʽ��
-        string newLine; // ����ַ��ռ�ÿһ�еķ�ע������
+        bool inBlockComment = false; // 当前是否在多行注释模式中
+        string newLine; // 逐个字符收集每一行的非注释内容
 
         for (const string& line : source) {
-            // ֻ���ڷǶ���ע��ģʽ�У��������newLine��
-            // ��Ϊ�ڶ���ע��ģʽ�У�newLine���ܰ�����Ч�Ĵ��룬���һ�û���ռ���ϣ�����Ҫ�����ռ������μ��ٷ�case2��
+            // 只有在非多行注释模式中，才能清空newLine；
+            // 因为在多行注释模式中，newLine可能包含有效的代码，而且还没有收集完毕，可能要跨行收集。（参见官方case2）
             if (!inBlockComment)
                 newLine = "";
 
-            for (int i = 0; i < line.length(); i++) { // ����ַ�ɨ��
+            for (int i = 0; i < line.length(); i++) { // 逐个字符扫描
                 if (!inBlockComment && i + 1 < line.length() && line[i] == '/' && line[i + 1] == '*') {
                     inBlockComment = true;
                     i++;
@@ -41,9 +41,9 @@ public:
                     inBlockComment = false;
                     i++;
                 } else if (!inBlockComment && i + 1 < line.length() && line[i] == '/' && line[i + 1] == '/') {
-                    break; // ֻ����������ע�ͷ���������ǰ����һ�е�ɨ�裻
+                    break; // 只有遇到单行注释符，才能提前结束一行的扫描；
                 } else if (!inBlockComment) {
-                    newLine += line[i]; // ����ַ��ռ�������������ע��ʱ������Ҫ�����ռ���
+                    newLine += line[i]; // 逐个字符收集；在遇到多行注释时，可能要跨行收集；
                 }
             }
 
