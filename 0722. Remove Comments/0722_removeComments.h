06cc8e9e29be4ec7a371c5dd9adb3f4b06b2da79
bigwindlee <bigwindlee@qq.com>
2024-10-26 02:43:00 +0800

    722. 删除注释

--- 0722. Remove Comments/0722_removeComments.h
+++ 0722. Remove Comments/0722_removeComments.h
@@ -1,8 +1,7 @@
-#pragma once
+﻿#pragma once
 #include <string>
 #include <vector>
 using namespace std;
-
 /*
 使用两个变量：inBlockComment 表示是否在多行注释中，newLine 用于构建当前行的内容。
 遍历每一行源代码，逐个字符处理：
@@ -19,7 +18,8 @@ using namespace std;
 3）遇单行注释符，提前结束一行的扫描；
 4）当前非注释，逐个字符收集；
 */
-class Solution_0722 {
+
+class Solution {
 public:
     vector<string> removeComments(vector<string>& source)
     {
@@ -30,17 +30,17 @@ public:
         for (const string& line : source) {
             // 只有在非多行注释模式中，才能清空newLine；
             // 因为在多行注释模式中，newLine可能包含有效的代码，而且还没有收集完毕，可能要跨行收集。（参见官方case2）
-            if (!inBlockComment)
+            if (!inBlockComment) {
                 newLine = "";
-
-            for (int i = 0; i < line.length(); i++) { // 逐个字符扫描
-                if (!inBlockComment && i + 1 < line.length() && line[i] == '/' && line[i + 1] == '*') {
+            }
+            for (int i = 0; i < line.length(); ++i) { // 逐个字符扫描
+                if (!inBlockComment && line.substr(i, 2) == "/*") {
                     inBlockComment = true;
-                    i++;
-                } else if (inBlockComment && i + 1 < line.length() && line[i] == '*' && line[i + 1] == '/') {
+                    ++i;
+                } else if (inBlockComment && line.substr(i, 2) == "*/") {
                     inBlockComment = false;
-                    i++;
-                } else if (!inBlockComment && i + 1 < line.length() && line[i] == '/' && line[i + 1] == '/') {
+                    ++i;
+                } else if (!inBlockComment && line.substr(i, 2) == "//") {
                     break; // 只有遇到单行注释符，才能提前结束一行的扫描；
                 } else if (!inBlockComment) {
                     newLine += line[i]; // 逐个字符收集；在遇到多行注释时，可能要跨行收集；
