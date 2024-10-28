53e4cf1c7b611908c687e3fd63b15295aacd8dd0
bigwindlee <bigwindlee@icloud.com>
2024-10-28 12:42:06 +0800

    Update 0015_threeSum.h

--- 0015. 3Sum/0015_threeSum.h
+++ 0015. 3Sum/0015_threeSum.h
@@ -5,7 +5,8 @@ using namespace std;
 
 /*
 15. 三数之和
-给你一个整数数组 nums ，判断是否存在三元组 [nums[i], nums[j], nums[k]] 满足 i != j、i != k 且 j != k ，同时还满足 nums[i] + nums[j] + nums[k] == 0 。请你返回所有和为 0 且不重复的三元组。
+给你一个整数数组 nums ，判断是否存在三元组 [nums[i], nums[j], nums[k]] 满足 i != j、i != k 且 j != k ，同时还满足 nums[i] + nums[j] + nums[k] == 0 。
+请你返回所有和为 0 且不重复的三元组。
 注意：答案中不可以包含重复的三元组。
 */
 
