e239e0c50494b21fdbd74739e641c0fb0dae416a
bigwindlee <bigwindlee@qq.com>
2024-10-30 18:35:31 +0800

    125. 验证回文串

---
+++ 0125. Valid Palindrome/0125_isPalindrome.h
@@ -0,0 +1,40 @@
+﻿#pragma once
+#include <cctype>
+#include <string>
+using namespace std;
+
+/*
+125. 验证回文串
+如果在将所有大写字符转换为小写字符、并移除所有非字母数字字符之后，短语正着读和反着读都一样。则可以认为该短语是一个 回文串 。
+字母和数字都属于字母数字字符。
+给你一个字符串 s，如果它是 回文串 ，返回 true ；否则，返回 false 。
+*/
+
+/*
+思路分析：双指针
+双指针分别从头尾对向移动，不使用额外的空间，空间复杂度为O(1)；
+
+要点：
+    熟悉<cctype>中的函数
+*/
+class Solution {
+public:
+    bool isPalindrome(string s)
+    {
+        int left = 0, right = s.size() - 1;
+        while (left < right) {
+            while (left < right && !isalnum(s[left])) {
+                ++left;
+            }
+            while (left < right && !isalnum(s[right])) {
+                --right;
+            }
+            if (left < right && tolower(s[left]) != tolower(s[right])) {
+                return false;
+            }
+            ++left;
+            --right;
+        }
+        return true;
+    }
+};
