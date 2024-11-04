e24fabac49f177d9de681a656f00b00c393f1756
bigwindlee <bigwindlee@qq.com>
2024-11-04 17:13:09 +0800

    98. 验证二叉搜索树

--- 0098. Validate Binary Search Tree/0098_isValidBST.h
+++ 0098. Validate Binary Search Tree/0098_isValidBST.h
@@ -1,6 +1,7 @@
 ﻿#pragma once
 #include "TreeNode.h"
 #include <climits>
+#include <functional>
 using namespace std;
 
 /*
@@ -16,6 +17,10 @@ using namespace std;
 /*
 思路分析：中序遍历
 BST中序遍历必须满足升序
+    
+要点：怎么解决卡边界值的问题？有2种方法：
+    1）使用LONG_MIN
+    2）使用节点指针记录上一个节点；
 */
 class Solution {
 public:
@@ -38,5 +43,32 @@ public:
     }
 
 private:
+    // 测试用例会卡边界值，例如BST中最小的值就是INT_MIN；有2种解决方案：
+    // 1）使用long类型的最小值；
+    // 2）使用节点指针，为空时不指向任何节点；
     long last = LONG_MIN;
+};
+
+class Solution_0098_02 {
+public:
+    bool isValidBST(TreeNode* root)
+    {
+        TreeNode* last = nullptr;
+        bool ans = true;
+        function<void(TreeNode*)> dfs = [&](TreeNode* root) {
+            if (!root) {
+                return;
+            }
+            dfs(root->left);
+            if (last && root->val <= last->val) { // 升序被打破
+                ans = false;
+                return;
+            }
+            last = root;
+            dfs(root->right);
+        };
+
+        dfs(root);
+        return ans;
+    }
 };
