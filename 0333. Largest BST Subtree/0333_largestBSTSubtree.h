ccf0c54b08450aa37789fc048bade6a51c52aa6c
bigwindlee <bigwindlee@qq.com>
2024-11-06 02:20:34 +0800

    333. 最大二叉搜索子树

--- 0333. Largest BST Subtree/0333_largestBSTSubtree.h
+++ 0333. Largest BST Subtree/0333_largestBSTSubtree.h
@@ -2,6 +2,7 @@
 #include "TreeNode.h"
 #include <algorithm>
 #include <climits>
+#include <functional>
 using namespace std;
 
 /* -------------------------------------------------------------------------------------------
@@ -17,7 +18,7 @@ using namespace std;
 
 /* -------------------------------------------------------------------------------------------
 思路分析：二叉树DFS
-    本题演示了使用DFS遍历二叉树，在回溯时把信息逐级上报并归拢的过程。
+    本题演示了使用DFS遍历二叉树，在回溯时把信息逐级上报并归拢的过程（dfs返回一个struct）。
 */
 
 /* -------------------------------------------------------------------------------------------
@@ -27,62 +28,62 @@ Node: 从下往上逐级上报的信息。
 
 左右子树在回溯的时候，把信息带到父节点。
 */
-struct Node {
-    int l, r, sz;
-};
-
 class Solution {
 public:
-    Node dfs(TreeNode* root, int& ans)
-    {
-        // DFS触达叶子节点，递归出口。
-        if (root->left == NULL && root->right == NULL) {
-            ans = max(ans, 1); // 叶子节点肯定是BST
-            return Node { root->val, root->val, 1 };
-        }
-
-        int sz = 1;
-        bool valid = 1;
-        int l = root->val,
-            r = root->val; // 起初的范围是[root->val,root->val]，再根据左右子树更新范围
-        if (root->left != NULL) {
-            Node L = dfs(root->left, ans);
-            if (L.sz != -1 && root->val > L.r) { // 左子树回溯
-                sz += L.sz;
-                l = L.l;
-            } else {
-                valid = 0;
-            }
-        }
-        if (root->right != NULL) {
-            Node R = dfs(root->right, ans);
-            if (R.sz != -1 && root->val < R.l) { // 右子树回溯
-                sz += R.sz;
-                r = R.r;
-            } else {
-                valid = 0;
-            }
-        }
-        if (valid) {
-            ans = max(ans, sz);
-            return Node { l, r, sz };
-        }
-        // 不是BST，sz设为-1标记不是BST，l,r多少都可以
-        return Node { -1, -1, -1 };
-    }
     int largestBSTSubtree(TreeNode* root)
     {
-        int ans = 0;
         if (!root) {
-            return ans;
+            return 0;
         }
-        dfs(root, ans);
+
+        int ans = 1;
+        function<Node(TreeNode*)> dfs = [&](TreeNode* root) {
+            // 起初的范围是[root->val,root->val]，再根据左右子树更新范围
+            int l = root->val, r = root->val, count = 1;
+            // DFS触达叶子节点，递归出口。
+            if (!root->left && !root->right) { // 叶子节点肯定是BST
+                return Node { l, r, count };
+            }
+
+            bool valid = true;
+            if (root->left) {
+                Node L = dfs(root->left);
+                if (L.count > 0 && l > L.r) { // 注意是和左子树的右边界比较
+                    l = L.l; // 刷新左边界
+                    count += L.count; // 刷新节点数量
+                } else {
+                    valid = false;
+                }
+            }
+            if (root->right) {
+                Node R = dfs(root->right);
+                if (R.count > 0 && r < R.l) { // 注意是和右子树的左边界比较
+                    r = R.r; // 刷新右边界
+                    count += R.count; // 刷新节点数量
+                } else {
+                    valid = false;
+                }
+            }
+
+            if (valid) {
+                ans = max(ans, count);
+                return Node { l, r, count };
+            }
+            return Node { -1, -1, -1 }; // count设为-1标记不是BST，l,r多少都可以
+        };
+
+        dfs(root);
         return ans;
     }
+
+private:
+    struct Node {
+        int l, r, count;
+    };
 };
 
 /*
-思路分析2：枚举
+思路分析2：枚举（低效）
 直观的想法就是我枚举每一个节点，去检查这个节点为根的子树是不是一棵二叉搜索树，如果是的话就统计一下这个子树的节点数并更新答案，
 否则就继续递归到左右子树去找有没有符合条件的二叉搜索树。
 那么先考虑如何判断一棵树是不是二叉搜索树，根据二叉搜索树的性质，我们可以从上往下递归判断。
